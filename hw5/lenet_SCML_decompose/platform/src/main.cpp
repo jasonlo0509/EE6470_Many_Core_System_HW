71238e17f611f5c78882294519dd24ddaef3d334
Jason Lo <tarzen0509@gmail.com>
2018-11-01 02:32:20 +0800

    hw5 finish

---
+++ hw5/lenet_SCML_decompose/platform/src/main.cpp
@@ -0,0 +1,15 @@
+#include "testbench/testbench.h"
+#include "acc_platform/acc_platform.h"
+
+// make testbench a global object
+Testbench testbench("testbench");
+
+int sc_main(int argc, char** argv) {
+	AccPlatform acc_platform("acc_platform");
+	testbench.i_socket(acc_platform.bus_t_socket_0);
+
+	sc_start();
+	std::cout<< "Simulated time:" << sc_core::sc_time_stamp() << std::endl;
+
+	return 0;
+}
